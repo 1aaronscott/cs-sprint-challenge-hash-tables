6887c3ed7f3cdf3e6f388587e296a2a4c0ae4ac3
1aaronscott <1aaronscott@users.noreply.github.com>
2020-06-04 16:04:12 -0500

    ex3 done

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,19 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    num_arrays = len(arrays)
+    cache = {}
+    for lst in arrays:
+        for elem in lst:
+            if elem in cache:
+                cache[elem] += 1
+            else:
+                cache[elem] = 1
+
+    result = []
+    for key in cache:
+        if cache[key] == num_arrays:
+            result.append(key)
 
     return result
 
