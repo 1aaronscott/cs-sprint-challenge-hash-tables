cc288182b43da93c72d7ac6dad3eb08f06c7e67d
1aaronscott <1aaronscott@users.noreply.github.com>
2020-06-04 16:33:47 -0500

    ex4 done

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,17 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    b = [0]*len(a)
+    cache = dict(zip(a, b))
+    result = []
+    for k in cache:
+        if k > 0:
+            #            print(k)
+            try:
+                if cache[k] is not None and cache[-k] is not None:
+                    result.append(k)
+            except:
+                pass
 
     return result
 
